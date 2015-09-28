231bb21c148bbb00ece9d957febb25a3f93a4edf
Tony Chung <anchung@cisco.com>
2015-09-28 00:18:25 -0400

    sample files

--- src/js/components/SampleContainer.js
+++ src/js/components/SampleContainer.js
@@ -3,6 +3,7 @@ var React = require('react');
 var SampleComponent = React.createClass({
 
     render: function(){
+        console.log('sample component');
         return (
             <div>
                 <h3>Hello World</h3>
