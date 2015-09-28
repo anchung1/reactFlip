04dbc003c97e0c2b9707e8c6365af4e5aa1d7745
Tony Chung <anchung@cisco.com>
2015-09-28 00:18:51 -0400

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,13 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify');
 var streamify = require('gulp-streamify');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
 
 var path = {
     HTML: 'src/index.html',
+    STYLES: 'src/styles/*.css',
     MINIFIED_OUT: 'build.min.js',
     OUT: 'build.js',
     DEST: 'dist',
@@ -18,19 +22,39 @@ var path = {
     PUBLIC: 'src/public/*'
 };
 
+gulp.task('buildCSS', function () {
+    gulp.src(path.STYLES).pipe(concat('main.css')).pipe(gulp.dest(path.DEST_SRC));
+});
+
+gulp.task('css', function () {
+    watch(path.STYLES, batch(function (events, done) {
+        gulp.start('buildCSS', done);
+    }));
+});
+
 gulp.task('replaceHTML', function() {
     gulp.src(path.HTML).pipe(htmlreplace({
         'js': 'src/' + path.OUT
     })).pipe(gulp.dest(path.DEST));
 });
 
+gulp.task('index', function() {
+    watch(path.HTML, batch(function(events, done) {
+        gulp.start('replaceHTML', done);
+    }));
+});
+
 gulp.task('copy', function() {
+    gulp.src(path.HTML).pipe(htmlreplace({
+        'js': 'src/' + path.OUT
+    })).pipe(gulp.dest(path.DEST));
+
     //gulp.src(path.HTML).pipe(gulp.dest(path.DEST));
     gulp.src(path.PUBLIC).pipe(gulp.dest(path.DEST_SRC));
+    gulp.src(path.STYLES).pipe(concat('main.css')).pipe(gulp.dest(path.DEST_SRC));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(path.HTML, ['replaceHTML']);
 
     var watcher = watchify( browserify( {
         entries: [path.ENTRY_POINT],
@@ -50,15 +74,6 @@ gulp.task('watch', function() {
         .pipe(gulp.dest(path.DEST_SRC));
 });
 
-// I added this so that you see how to run two watch tasks
-gulp.task('css', function () {
-    gulp.watch('styles/**/*.css', function () {
-        return gulp.src('styles/**/*.css')
-            .pipe(concat('main.css'))
-            .pipe(gulp.dest('build/'));
-    });
-});
-
 //use this command to find errors in build
 //browserify -t reactify -r react -r ./src/js/App.js > dist/src/bundle.js
-gulp.task('default', ['watch', 'replaceHTML', 'copy']);
+gulp.task('default', ['watch', 'copy', 'css', 'index']);
