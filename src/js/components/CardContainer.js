3fae2c4a12c6e6ecd6d51948bf8dda7d8222becc
Tony Chung <anchung@gmail.com>
2015-10-02 01:38:18 -0400

    two cards flip

--- src/js/components/CardContainer.js
+++ src/js/components/CardContainer.js
@@ -1,59 +1,30 @@
 var React = require('react');
 
 var CardContainer = React.createClass({
-//<div className="deck AceSpade"></div>
-//<div className="deck KingHeart"></div>
-        //<div className="deck Joker"></div>
-        //<div className="deck Back"></div>
 
+    aceFlip: function() {
+        $('#cardAce').toggleClass('flipped');
+    },
 
-/*<section className="container">
-    <div id="card" onClick={this.click}>
-        <figure className="front">1</figure>
-        <figure className="back">2</figure>
-    </div>
-</section>*/
-
-
-    click: function() {
-        console.log('clicked');
-        $('#card').toggleClass('flipped');
+    kingFlip: function() {
+        $('#cardKing').toggleClass('flipped');
     },
 
     render: function () {
 
-        var threeSpade = {
-            position: 'absolute',
-            top: '50px',
-            left: '450px',
-            clip: 'rect(24px, 549px, 243px, 390px)'
-        };
-
-        var AceSpade = {
-            position: 'absolute',
-            top: '-350px',
-            left: '182px',
-            clip: 'rect(412px, 183px, 654px, 9px)'
-        };
-
-        var CardBack = {
-            position: 'absolute',
-            top: '-350px',
-            left: '-182px',
-            clip: 'rect(412px, 729px, 654px, 555px)'
-        };
-
-        var simpleStyle = {
-            background: 'url(src/deck.jpg)'
-        };
         return (
 
-            <section className="container">
-                <div id="card" onClick={this.click}>
-                    <div className="front deck"></div>
-                    <div className="back deck"></div>
+            <div className="container">
+                <div className="card" id="cardAce" onClick={this.aceFlip} style={{top: '10%', left: '20%'}}>
+                    <div id="Ace" className="deck front"></div>
+                    <div className="deck back"></div>
                 </div>
-            </section>
+
+                <div className="card" id="cardKing" onClick={this.kingFlip} style={{top: '10%', left: '45%'}}>
+                    <div id="King" className="deck front"></div>
+                    <div className="deck back"></div>
+                </div>
+            </div>
 
 
         );
